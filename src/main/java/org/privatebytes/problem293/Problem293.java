9fed7ea59169af4f9cac53e8ba69e9b260bb77ef
Bogdan Ungureanu <b.ungureanu@oodrive.com>
2019-06-10 10:29:56 +0300

    problem 293

---
+++ src/main/java/org/privatebytes/problem293/Problem293.java
@@ -0,0 +1,23 @@
+package org.privatebytes.problem293;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem293 {
+
+	public static void main(String[] args) {
+		String stones = "[1, 1, 3, 3, 2, 1]";
+
+		int cost = new Problem293().solve(stones);
+
+		System.out.println("Pyramid cost: " + cost);
+	}
+
+	public int solve(String stones) {
+		//TODO: add code here
+		return 0;
+	}
+
+
+}
